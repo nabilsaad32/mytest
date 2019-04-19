8ca929398764583d9aa97bd161da8e8fde2730cd
Nabil m.  Saad <nabilsaad32@gmail.com>
2019-04-19 07:17:07 +0100

    Update logger.py

--- logger.py
+++ logger.py
@@ -6,4 +6,4 @@ logformat = "%(levelname)s %(asctime)s -  %(message)s"
 
 logging.basicConfig(filename = '/home/sally/logger/info.log',level = logging.INFO ,format = logformat)
 logger  = logging.getLogger()
-logger.info("إغتصاب الطفلات أقل من 6 أشهر")
+logger.info("test")
